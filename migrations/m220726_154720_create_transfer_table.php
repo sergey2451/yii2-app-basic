2233d7cb0bbc065ceb37a90cc5adcd962fe83200
sergey2451 <kragel.s@mail.ru>
2022-07-26 20:16:05 +0300

    create transfer table migration

--- migrations/m220726_154720_create_transfer_table.php
+++ migrations/m220726_154720_create_transfer_table.php
@@ -7,24 +7,24 @@ use yii\db\Migration;
  */
 class m220726_154720_create_transfer_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-        $this->createTable('{{%transfer}}', [
-            'id' => $this->primaryKey(),
-						'from_account_id' => $this->integer()->notNull(),
-						'to_account_id' => $this->integer()->notNull(),
-						'sum' => $this->decimal(10,2)->null() . ' DEFAULT NULL',
-        ]);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp()
+	{
+		$this->createTable('{{%transfer}}', [
+			'id' => $this->primaryKey(),
+			'from_account_id' => $this->integer()->notNull(),
+			'to_account_id' => $this->integer()->notNull(),
+			'sum' => $this->decimal(10, 2)->null() . ' DEFAULT NULL',
+		]);
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        $this->dropTable('{{%transfer}}');
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown()
+	{
+		$this->dropTable('{{%transfer}}');
+	}
 }
